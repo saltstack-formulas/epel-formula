35b98d55c8ea4b786a889e33bc0418d2f2d87dbe
Imran Iqbal <iqbalmy@hotmail.com>
2019-11-03 04:34:39 +0000

    test(inspec): provide tests for the repo package and config

---
+++ test/integration/default/controls/config_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+control 'epel repo configuration' do
+  title 'should match desired lines'
+
+  epel_version =
+    case platform[:name]
+    when 'amazon'
+      case platform[:release]
+      when '2'
+        '7'
+      when '2018.03'
+        '6'
+      end
+    when 'centos'
+      platform[:release][0]
+    end
+
+  describe file("/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-#{epel_version}") do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+    its('mode') { should cmp '0644' }
+  end
+end
